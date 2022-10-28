107e9bb8ccd2075791a9f29d6a159fa0e94a67fb
Saqib <iamsaqib24@gmail.com>
2022-10-28 14:35:35 +0530

    html & js done

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const dateOfBirth = document.querySelector('#date-of-birth');
 const luckyNumber = document.querySelector('#lucky-number');
 const checkNumber = document.querySelector('#check-number');
+const outputBox = document.querySelector('#output-box');
+
+function comparesValues(sum, luckyNumber) {
+    if(sum % luckyNumber === 0){
+        outputBox.innerText = 'Your birthday is lucky 😁';
+    }
+    else{
+        outputBox.innerText = 'Your birthday is not lucky 😔';
+    }
+}
 
 function checkNumberIsLucky() {
     const birthDate = dateOfBirth.value;
     const sum = calculateSum(birthDate);
-    console.log(sum);
+    if (sum && birthDate) {
+        comparesValues(sum, luckyNumber.value)
+    } else {
+        outputBox.innerText = "Please, enter both fields.";
+    }
 }
 
-function calculateSum(date) {
+function calculateSum(birthDate) {
     birthDate = birthDate.replaceAll('-','');
     let sum = 0;
     for (let i = 0; i < birthDate.length; i++) {
